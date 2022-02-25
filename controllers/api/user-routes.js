c1a573a6cb40d2e76c668e11800b565282db1a97
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-02-25 11:09:01 -0800

    store hashed password on post and put request

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -51,11 +51,12 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
-      where: {
+        individualHooks: true,
+        where: {
         id: req.params.id
-      }
+        }
     })
       .then(dbUserData => {
         if (!dbUserData[0]) {
