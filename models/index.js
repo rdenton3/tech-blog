889402dd133b868dbe9a7c54a50da6041e8ac1aa
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-03-01 10:47:27 -0500

    post model and api requests finished

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
-const User = require('./User');
-const Post = require('./Post');
+const User = require("./User");
+const Post = require("./Post");
 
 module.exports = { User, Post };
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+Post.belongsTo(User, {
+foreignKey: 'user_id',
+});
