6038f5eb541e299d5f708e5c30132abcea86b558
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-03-02 11:21:41 -0500

    user has ability to comment on single post

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,20 +2,26 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
+    Comment.findAll({
 
+    })
 });
 
 router.post('/', (req, res) => {
+  // check the session
+  if (req.session) {
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 router.delete('/:id', (req, res) => {
